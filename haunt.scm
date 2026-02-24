d0b985932f4730af8280ab4e9053e6a1ec339dd4
Nicholas McDaniel <nmcdaniel@tty.garden>
2026-02-24 00:05:33 -0500

    Initial design

--- haunt.scm
+++ haunt.scm
@@ -5,28 +5,39 @@
   (haunt builder assets)
   (haunt builder flat-pages)
   (haunt reader)
+  (haunt reader skribe)
   (haunt site)
 
   (nursery builders commandline))
 
 (define (nursery-template-base site metadata body)
+  (define display-title
+    (or (assq-ref metadata 'title) "tty.garden"))
   `((doctype "html")
     (head
      (meta (@ (charset "utf-8")))
-     (title ,(or (assq-ref metadata 'title) "tty.garden"))
+     (title ,display-title)
      (link (@
             (rel "stylesheet")
             (href "/core.css"))))
     (body
      (div (@
            (id "content"))
+      (h1 (@ (class "title")) ,display-title)
+      (nav
+        (@ (class "primary-nav"))
+        (a (@ (href "/")) "Home" )
+        (div (@ (class "vertical-seperator")))
+        (a (@ (href "https://seed.tty.garden/nmcdaniel")) "Git"))
       ,body))))
 
 (site #:title "tty garden"
   #:domain
   "tty.garden"
   #:readers
-  (list sxml-reader)
+  (list
+    sxml-reader
+    skribe-reader)
   #:builders
   (list
     (cmd-builder '("sass" "--style=compressed") "css/core.scss" "core.css")
